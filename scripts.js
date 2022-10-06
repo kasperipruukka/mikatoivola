3a69518864c70d6162ed52cd6a18756b619df19e
Kasperi Pruukka <kasperi.pruukka@gmail.com>
2022-10-06 18:26:15 +0300

    Linkkien aktiivisuus-toiminnon kehitys.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+const setLinkActive = (id) => {
+    const link = document.getElementById(id);
+    if (!link) return;
+
+    const activeLink = document.querySelector('.active');
+    if (!activeLink) return;
+    
+    activeLink.classList.remove('active');
+    link.classList.add('active');
+} 
+
 const toggleBtnVisibility = (btnId) => {
     const btn = document.getElementById(btnId);
     if (!btn) return;
